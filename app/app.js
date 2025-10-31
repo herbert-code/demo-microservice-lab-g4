88872b340bffc578ac41c46c39b5d6a39e2e62dd
herbert-code <herbert.code@gmail.com>
2025-10-31 00:38:57 -0400

    Actualizacion de prueba con mensaje de Microservicio 4

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (_req, res) => {
-  res.json({ status: "ok", service: "Hola Microservicio 3" });
+  res.json({ status: "ok", service: "Hola Microservicio 4" });
 });
 
 export default app;
